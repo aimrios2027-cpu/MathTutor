adc2f6242dbe1e2cf4f2ac8f04adfc37c816f7b0
Aimee Rios <aimrios2027@gmail.com>
2026-01-29 10:33:24 -0800

    Initial Commit

---
+++ MathTutor/MathTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathTutorApp.swift
+//  MathTutor
+//
+//  Created by RIOS, AIMEE on 1/29/26.
+//
+
+import SwiftUI
+
+@main
+struct MathTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
