c767827046ddb035ccca02fe70a82286dd96bbc7
Aimee Rios <aimrios2027@gmail.com>
2026-01-29 11:17:32 -0800

    save changes

--- MathTutor/ContentView.swift
+++ MathTutor/ContentView.swift
@@ -12,21 +12,43 @@ struct ContentView: View {
     @State private var secondNumber = 0
     @State private var firstNumberEmojis = ""
     @State private var secondNumberEmojis = ""
+    @State private var answer = ""
     
     private let emojis = ["🍕", "🍎", "🍏", "🐵", "👽", "🧠", "🧜🏽‍♀️", "🧙🏿‍♂️", "🥷", "🐶", "🐹", "🐣", "🦄", "🐝", "🦉", "🦋", "🦖", "🐙", "🦞", "🐟", "🦔", "🐲", "🌻", "🌍", "🌈", "🍔", "🌮", "🍦", "🍩", "🍪"]
     
     var body: some View {
         VStack {
-            Text(firstNumberEmojis)
-                .font(Font.system(size: 80))
+            Group {
+                Text(firstNumberEmojis)
+                Text("+")
+                Text(secondNumberEmojis)
+            }
+            .font(Font.system(size: 80))
+            .multilineTextAlignment(.center)
+            .minimumScaleFactor(0.5)
+                
+            Spacer()
+            
             Text("\(firstNumber) + \(secondNumber) =")
                 .font(.largeTitle)
+            
+            TextField("", text: $answer)
+                .font(.largeTitle)
+                .frame(width:60)
+                .textFieldStyle(.roundedBorder)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(.gray, lineWidth: 2)
+                }
+                .multilineTextAlignment(.center)
+                .keyboardType(.numberPad)
         }
         .padding()
         .onAppear {
             firstNumber = Int.random(in: 1...10)
             secondNumber = Int.random(in: 1...10)
             firstNumberEmojis = String(repeating: emojis.randomElement()!, count: firstNumber)
+            secondNumberEmojis = String(repeating: emojis.randomElement()!, count: firstNumber)
         }
     }
 }
